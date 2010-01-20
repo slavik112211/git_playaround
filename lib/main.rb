11ff83ae4ed4f03430c35d2adea87bb966eb3431
Slavik Derevyanko <slavik2121@gmail.com>
2010-01-20 14:13:49 +0100

    playing with testing

--- lib/main.rb
+++ lib/main.rb
@@ -6,11 +6,8 @@ puts "Hello World"
 puts "Edit of previous existing file"
 
 
-<<<<<<< Updated upstream:lib/main.rb
 puts "another chanzzzzzzzzzzzzzzzzzzzzzzzzzzzzzge.........some more changes in master"
-=======
-puts "another change  *((**((*((((((some changes)))))).........some more changes in master"
->>>>>>> Stashed changes:lib/main.rb
+
 
 
 puts "working in branch   !here is the edit of that!"
